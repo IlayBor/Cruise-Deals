171b76c050465931d0b9aad73ed547d08f753ac0
IlayBor <ilaybor2004@gmail.com>
2026-04-09 23:40:56 +0300

    changed to use more generic code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
 LOGIN_URL = "https://www.vacationstogo.com/login.cfm?t=y"
 DEALS_URL = "https://www.vacationstogo.com/ticker.cfm?t=y&sp=y"
 EMAIL = "ilaybor2004@gmail.com"
@@ -26,30 +26,20 @@ EMAIL = "ilaybor2004@gmail.com"
 soup = BeautifulSoup(open("deals.html"), 'html.parser')
 
 deals_tables = soup.find_all("table", class_="ticker deals")
-
+total_rows = []
 for deal_table in deals_tables:
     region_name = deal_table.find_previous_sibling("table", class_="ticker region").get_text(strip=True)
 
     rows = deal_table.find_all("tr")
-    for row in rows:
-        fd = row.find("td", class_="fd").get_text(strip=True)
-        n = row.find("td", class_="n").get_text(strip=True)
-        d = row.find("td", class_="d").get_text(strip=True)
-        e = row.find("td", class_="e").get_text(strip=True)
-        ls = row.find("td", class_="ls").get_text(strip=True)
-        r = row.find("td", class_="r").get_text(strip=True)
-        r = row.find("td", class_="r").get_text(strip=True)
-        br = row.find("td", class_="br").get_text(strip=True)
-        our = row.find("td", class_="our").get_text(strip=True)
-        p = row.find("td", class_="p").get_text(strip=True)
+    row_keys = ["fd", "n", "d", "e", "ls", "r", "br", "our", "p", "st"]
 
-        st = row.find("td", class_="st").get_text(strip=True)
-        st_suite = row.find("td", class_="st suite").get_text(strip=True)
-        st_onboard_credit = row.find("td", class_="st onboard-credit").get_text(strip=True)
-        st_resident_rate = row.find("td", class_="st resident-rate").get_text(strip=True)
-        st_oceanview = row.find("td", class_="st oceanview").get_text(strip=True)
-        st_reduced_again = row.find("td", class_="st reduced-again").get_text(strip=True)
-        st_sold_out = row.find("td", class_="st sold-out").get_text(strip=True)
-        st_new_offer = row.find("td", class_="st new-offer").get_text(strip=True)
-        st_balcony = row.find("td", class_="st balcony").get_text(strip=True)
-        
+    for row in rows:
+        row_data = {}
+        for key in row_keys:
+            td = row.find("td", class_=key)
+            row_data[key] = td.get_text(strip=True) if td else None
+        row_data["region"] = region_name
+        total_rows.append(row_data)
+
+df = pd.DataFrame(total_rows)
+print(df.head())

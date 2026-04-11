cc3687ce15a90336b1f630dd8d4bc64c9e2ae6cc
IlayBor <ilaybor2004@gmail.com>
2026-04-11 12:14:23 +0300

    fixed types and added calculated column

--- dbt_project/models/staging/stg_scrape__deals.sql
+++ dbt_project/models/staging/stg_scrape__deals.sql
@@ -1,15 +1,50 @@
-select 
-    "fd" as "id",
-    "fd_url" as "url",
-    "n" as "number_of_nights",
-    "dt" as "departure_date",
-    "d" as "departs_from",
-    "e" as "ends_at",
-    "ls" as "cruise_line_ship",
-    "r" as "ship_rating",
-    "br" as "normal_price",
-    "our" as "current_price",
-    "p" as "percentage_off",
-    "st" as "status",
-    "region" as "region"
-from {{ source('postgres', 'deals') }}
+with source as (
+    select 
+        "fd",
+        "fd_url",
+        "n",
+        "dt",
+        "d",
+        "e",
+        "ls",
+        "r",
+        "br",
+        "our",
+        "st",
+        "region"
+    from {{ source('postgres', 'deals') }}
+),
+renamed as (
+    select 
+        REPLACE("fd", '#', '') as "id",
+        "fd_url" as "url",
+        "n"::INT as "number_of_nights",
+        CASE 
+            WHEN TO_DATE("dt" || ' ' || EXTRACT(YEAR FROM CURRENT_DATE), 'Mon DD YYYY') < CURRENT_DATE
+            THEN TO_DATE("dt" || ' ' || EXTRACT(YEAR FROM CURRENT_DATE), 'Mon DD YYYY') + INTERVAL '1 year'
+            ELSE TO_DATE("dt" || ' ' || EXTRACT(YEAR FROM CURRENT_DATE), 'Mon DD YYYY')
+        END::DATE AS "departure_date",
+        split_part("d", ',', 2) as "departs_from_state",
+        split_part("d", ',', 1) as "departs_from_city",
+        split_part("e", ',', 2) as "ends_at_state",
+        split_part("e", ',', 1) as "ends_at_city",
+        split_part("ls", '/', 1) as "cruise_line",
+        split_part("ls", '/', 2) as "ship",
+        "r"::FLOAT as "ship_rating",
+        nullif(replace(replace("br", '$', ''), ',', ''), '-')::NUMERIC as "normal_price",
+        nullif(replace(replace("our", '$', ''), ',', ''), '-')::NUMERIC as "current_price",
+        "st" as "status",
+        "region"
+    from source
+),
+calculated as (
+    select
+        *,
+        ("departure_date" + "number_of_nights" * INTERVAL '1 day')::DATE as "end_date",
+        ROUND((1 - "current_price" / NULLIF("normal_price", 0)) * 100, 1) as "percentage_off"
+    from renamed
+)
+
+select *
+from calculated
+i wan
